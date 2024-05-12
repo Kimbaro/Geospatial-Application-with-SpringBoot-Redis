5625aa22f131aa7a6a942f449555cd183a6976cf
Kimbaro <azxcv1329@gmail.com>
2024-05-12 22:23:50 +0900

    :tada: init project

---
+++ src/main/java/com/template/redis/geospatialapplicationwithspringbootredis/GeospatialApplicationWithSpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.template.redis.geospatialapplicationwithspringbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeospatialApplicationWithSpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeospatialApplicationWithSpringBootRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/template/redis/geospatialapplicationwithspringbootredis/GeospatialApplicationWithSpringBootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.redis.geospatialapplicationwithspringbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeospatialApplicationWithSpringBootRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
